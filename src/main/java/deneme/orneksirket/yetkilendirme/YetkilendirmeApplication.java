6ca87de9b0c11362c258cc016fd4a83438d51bb4
taha.sozgen <taha.sozgen@BID023.yok82.yerel>
2024-01-05 11:08:47 +0300

    ilk commit

---
+++ src/main/java/deneme/orneksirket/yetkilendirme/YetkilendirmeApplication.java
@@ -0,0 +1,13 @@
+package deneme.orneksirket.yetkilendirme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetkilendirmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetkilendirmeApplication.class, args);
+	}
+
+}
