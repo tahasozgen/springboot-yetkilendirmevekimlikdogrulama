14136c0a73a7e3899195a38ce668e02eb715245e
taha.sozgen <taha.sozgen@BID023.yok82.yerel>
2024-01-05 11:07:53 +0300

    ilk commit

---
+++ src/main/java/deneme/orneksirket/yetkilendirme/konfigurasyon/WebSecurityConfigurator.java
@@ -0,0 +1,7 @@
+package deneme.orneksirket.yetkilendirme.konfigurasyon;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration; 
+ 
+public class WebSecurityConfigurator  {
+
+}
